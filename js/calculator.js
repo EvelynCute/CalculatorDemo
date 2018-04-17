388d01eeeaa2d7b5531d38964a20bc95daa7c596
EvelynCute <38399932+EvelynCute@users.noreply.github.com>
2018-04-17 21:53:22 +0800

    添加了计算器的运算功能，但有一个小bug未解决

---
+++ js/calculator.js
@@ -0,0 +1,35 @@
+var mark = false;
+function get(key){
+	// alert(key);
+	var str=document.getElementById('output').value;
+	
+	if (str==0) {
+		str='';
+	}
+
+	if(mark==false){
+		str = str+key;
+	}else{
+		str = key;
+	}
+	
+
+	document.getElementById('output').value=str;
+
+}
+function eq(){
+	// /alert(1);
+	mark= true;
+	var str=document.getElementById('output').value;
+	// eval(str);
+	document.getElementById('output').value=eval(str);
+}
+function clearText(){
+	
+	document.getElementById('output').value="0";
+}
+function clearOneText(){
+	var str=document.getElementById('output').value;
+	str = str.substring(0,str.length-1);
+	document.getElementById('output').value=str;
+}
